464f2837d6317d1f8c3aa09d3aa028997dd1000a
Maycon Ricardo Oliveira <PUCMINAS\1051701@PPC01INFLL00127.pucminas.net>
2020-02-13 22:04:30 -0300

    #1: Trys by sort

--- game.rb
+++ game.rb
@@ -7,8 +7,14 @@ puts "Escolhendo um numero secreto entre 0 e 200..."
 numero_secreto = 121
 puts "Escolhido... que tal adivinhar hoje o nosso numero secreto?"
 puts "\n\n\n"
-puts "Tentativa 1"
-puts "Entre com o numero"
-chute = gets
-puts "Sera que acertou? Voce chutou " + chute
-puts numero_secreto == chute.to_i
+
+$initial = 0
+$numerotentativa = 3
+while $initial < $numerotentativa  do
+    puts("Tentativa #$numerotentativa" )
+    puts "Entre com o numero"
+    chute = gets
+    puts "Sera que acertou? Voce chutou " + chute
+    puts numero_secreto == chute.to_i
+    $initial +=1
+ end
